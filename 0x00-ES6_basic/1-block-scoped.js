45e6ce4dad27f90b2e836770f323438e6326f4d0
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-22 15:18:54 -0700

    FrontEnd_JS

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,7 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+   return [task, task2];
   }
   return [task, task2];
 }
