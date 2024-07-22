347e87b32de3569151a91d128d20a8ad531f0d50
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-22 16:27:29 -0700

    FrontEnd_JS

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
+}
