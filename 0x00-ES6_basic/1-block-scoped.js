2091b2024b94e29351f1959eb97e00ea05be9242
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-22 15:53:31 -0700

    FrontEnd_JS

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
-#!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-   return [task, task2];
+    return [task, task2];
   }
   return [task, task2];
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 export default function returnHowManyArguments(...args) {
     return args.length;
 }
