1f00cc02341f113ad088f08617b8cca42b024821
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-24 08:18:01 -0700

    FrontEnd_JS

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+export default function getResponseFromAPI() {
+    const success = true;
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+}
