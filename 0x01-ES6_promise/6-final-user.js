847e947fe8b89c3583be7296e8601ed9dce63aa2
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-24 12:35:28 -0700

    FrontEnd_JS

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+import signUpUser from `./4-user-promise.js`;
+import uploadPhoto from `./5-photo-reject`
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    import function name(params) {
+        
+    }
+    return Promise.allSettled([
+        {
+          status: status_of_the_promise,
+          value: value or error returned by the Promise
+        },
+      ]).then((values) => {
+        const arr = [];
+        for (const item of values) {
+          arr.push({ status: item.status, value: item.value || item.reason });
+        }
+        return arr;
+      });
+}
