28841421746584e8328667473956bb7859fec836
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-26 14:28:06 -0700

    FrontEnd_JS

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const ModelCar = this.constructor[Symbol.species];
+    return new ModelCar();
+  }
+}
