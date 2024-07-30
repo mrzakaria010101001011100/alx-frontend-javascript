1df2c79a96c4e3224e1bf7ce532f6353ce1f33f3
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-30 05:48:40 -0700

    FrontEnd_JS_array_arrayeverywhere

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,19 @@
-function getListStudents() {
-    return [{
-        id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-    {
-        id: 2, firstName: 'James', location: 'Columbia'},
-    {
-        id: 5, firstName: 'Serena', location: 'San Francisco',}
-]
-}
+export default function getListStudents() {
+    const Arr1 = {
+        id: 1,
+        firstName: 'Guillaume',
+        location: 'San Francisco',
+    };
+    const Arr2 = {
+        id: 2,
+        firstName: 'James',
+        location: 'Columbia',
+    };
+    const Arr3 = {
+        id: 5,
+        firstName: 'Serena',
+        location: 'San Francisco',
+    };
 
-getListStudents()
+    return [Arr1, Arr2, Arr3];
+}
