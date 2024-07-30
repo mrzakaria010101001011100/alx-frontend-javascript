429a1c19c9e8e6636140ca033213a7d7675d9c36
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-30 13:28:25 -0700

    FrontEnd_JS_array_arrayeverywhere

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,21 @@
 export default function getListStudents() {
-    const Arr1 = {
-        id: 1,
-        firstName: 'Guillaume',
-        location: 'San Francisco',
-    };
-    const Arr2 = {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia',
-    };
-    const Arr3 = {
-        id: 5,
-        firstName: 'Serena',
-        location: 'San Francisco',
-    };
+  const o1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
 
-    return [Arr1, Arr2, Arr3];
+  const o2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const o3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [o1, o2, o3];
 }
--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function getListStudentIds(arr) {
+  let newArr = [];
+  if (arr instanceof Array) {
+    newArr = arr.map((item) => item.id);
+  }
+
+  return newArr;
+}
