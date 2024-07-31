cceaad7d4e7c00f3abc5748f0a575cadc354315f
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-31 14:09:37 -0700

    FrontEnd_JS_array_arrayeverywhere

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,49 @@
-interface  attributes {
+// 1. Teacher Interface
+interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    contract: boolean;
-    [propName: string]: any;
-}
+    [propName: string]: any; // To allow any additional properties
+  }
+  
+  // 2. Print Teacher Function
+  interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+  
+  // 3. StudentClass Interfaces
+  interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+  }
+  
+  interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  // 4. Implementing StudentClass
+  class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+}  
