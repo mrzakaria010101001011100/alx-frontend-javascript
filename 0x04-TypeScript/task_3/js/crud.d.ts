329cbfe13173bb5051aa7cd0de448ef1c08f3103
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-31 15:01:50 -0700

    FrontEnd_JS_array_arrayeverywhere

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,9 @@
+
+import { RowID, RowElement } from './interface';
+
+declare module CRUD {
+  function insertRow(row: RowElement): RowID;
+  function deleteRow(rowId: RowID): void;
+  function updateRow(rowId: RowID, row: RowElement): RowID;
+}
+
---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,27 @@
+// task_3/js/main.ts
+
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+console.log(`Insert row`, row);
+
+const updatedRow: RowElement = { 
+  firstName: 'Guillaume', 
+  lastName: 'Salva', 
+  age: 23 
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+console.log(`Update row ${newRowID}`, updatedRow);
+
+CRUD.deleteRow(newRowID);
+console.log(`Delete row id ${newRowID}`);
+
