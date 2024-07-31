aee10cca80834773a7b7a78e65bf1bb696c003d6
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-07-31 15:18:28 -0700

    FrontEnd_JS_array_arrayeverywhere

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,26 @@
+// task_5/js/main.ts
+
+interface MajorCredits {
+  credits: number;
+  _majorBrand: 'MajorCredits';
+}
+
+interface MinorCredits {
+  credits: number;
+  _minorBrand: 'MinorCredits';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    _majorBrand: 'MajorCredits',
+  };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    _minorBrand: 'MinorCredits',
+  };
+}
+
